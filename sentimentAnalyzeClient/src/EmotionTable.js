5da5a499e40becd704a127ccf41ec6bb7c678bc7
Bharat <anchawalebharat203@gmail.com>
2022-10-02 12:46:36 -0400

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -12,6 +12,8 @@ class EmotionTable extends React.Component {
               /*Write code to use the .map method that you worked on in the 
               Hands-on React lab to extract the emotions. If you are stuck,
               please click the instructions to see how to implement a map*/
+
+              
             }
             </tbody>
           </table>
